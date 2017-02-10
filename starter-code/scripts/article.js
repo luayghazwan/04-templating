d767f14fe60898a276ba13877438f306843a8839
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-02-10 12:05:03 -0800

    Use handlebars to render articles, return HTML

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -12,10 +12,11 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
+  // DONE: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-
+  var source = $('#articles-handleBars').html();
+  var templateCompiler = Handlebars.compile(source);
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -23,8 +24,8 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
-  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-
+  // DONE: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  return templateCompiler(this);
 };
 
 rawData.sort(function(a,b) {
